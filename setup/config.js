319fe0f182d86105b8271515a8a1acefaa7cca1c
Aditya <53331534+addy123d@users.noreply.github.com>
2021-04-24 07:44:14 +0530

    Update config.js

--- setup/config.js
+++ setup/config.js
@@ -1,5 +1,5 @@
 const setting = {
-    url: 'mongodb+srv://aditya123d:Zz12CeUxnzUGK7Qs@cluster0.41gfj.mongodb.net/GITHUBCLONE?retryWrites=true&w=majority',
+    url: 'mongodb+srv://username:password@cluster0.41gfj.mongodb.net/GITHUBCLONE?retryWrites=true&w=majority',
     secret: "mysecret"
 }
 
